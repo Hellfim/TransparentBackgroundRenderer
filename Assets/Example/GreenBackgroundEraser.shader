af31ad03556190b8f9e0c1f0df0ad5174fa742e6
Hellfim <hellyforex@gmail.com>
2023-05-21 19:54:02 +0300

    Added script and shader

---
+++ Assets/Example/GreenBackgroundEraser.shader
@@ -0,0 +1,37 @@
+﻿Shader "GreenBackgroundEraser"
+{
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" "RenderPipeline" = "UniversalPipeline"}
+        LOD 100
+        ZWrite Off Cull Off
+        Pass
+        {
+            Name "GreenBackgroundEraserPass"
+
+            HLSLPROGRAM
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+            // The Blit.hlsl file provides the vertex shader (Vert),
+            // input structure (Attributes) and output strucutre (Varyings)
+            #include "Packages/com.unity.render-pipelines.core/Runtime/Utilities/Blit.hlsl"
+
+            #pragma vertex Vert
+            #pragma fragment frag
+            
+            sampler2D _Texture;
+
+            float get_inverted_color_mask(const float3 color)
+            {
+                const float3 mask_color = float3(0, 1, 0);
+                return abs(1 - saturate(1 - (distance(mask_color, color) - 0.4f) / 1e-5));
+            }
+
+            half4 frag (Varyings input) : SV_Target
+            {
+                const float non_green_screen_alpha = get_inverted_color_mask(tex2D(_Texture, input.texcoord).xyz);
+                return half4(tex2D(_Texture, input.texcoord).xyz, non_green_screen_alpha);
+            }
+            ENDHLSL
+        }
+    }
+}
---
+++ Assets/Example/TransparentBackgroundRenderer.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Rendering
+{
+    public class TransparentBackgroundRenderer : MonoBehaviour
+    {
+        private static RTHandle _targetRTHandle;
+        
+        [SerializeField]
+        private Camera _camera;
+        
+        [SerializeField]
+        private Shader _shader;
+        
+        [SerializeField]
+        private RenderTexture _targetTexture;
+
+        private Material _material;
+        private RTHandle _sourceRTHandle;
+        private RenderTexture _sourceTexture;
+
+        private void Awake()
+        {
+            _material = CoreUtils.CreateEngineMaterial(_shader);
+            if (_targetTexture != null)
+            {
+                _targetRTHandle ??= RTHandles.Alloc(_targetTexture);
+
+                _sourceTexture = new RenderTexture(_targetTexture.descriptor);
+                _sourceRTHandle = RTHandles.Alloc(_sourceTexture);
+                
+                _camera.targetTexture = _sourceTexture;
+                
+                _material.SetTexture(Shader.PropertyToID("_Texture"), _sourceTexture);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            /*
+             * _targetRTHandle should be released only if it's not an asset reference and created dynamically (like sourceTexture here).
+             * Static asset objects are destroyed in builds (in the Editor it's fine) when RTHandle gets released, so if you release _targetRTHandle,
+             * reload scene with this script and try to to use _targetTexture you will get 'null' as _targetTexture asset will be removed in-game
+             *
+             * That's also the sole reason why _targetTexture has static modifier in this script
+             */
+            RTHandles.Release(_sourceRTHandle);
+            //No need to manually destroy _sourceTexture here, because RTHandles.Release(_sourceRTHandle) call destroys it internally
+        }
+
+        private void LateUpdate()
+        {
+            var cmd = CommandBufferPool.Get();
+
+            Blitter.BlitCameraTexture(cmd, _sourceRTHandle, _targetRTHandle, _material, 0);
+
+            Graphics.ExecuteCommandBuffer(cmd);
+            cmd.Clear();
+        }
+    }
+}
